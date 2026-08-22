eb71abb14af089f0eaf927551287b356d9655918
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-08-22 03:47:52 -0400

    add: pet routes

---
+++ backend/src/routes/pet.routes.js
@@ -0,0 +1,19 @@
+"use strict";
+import { Router } from "express";
+import { isAdmin } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
+import { createPet, deletePet, getManyPets, getPet, updatePet } from "../controllers/pet.controller.js";
+
+const router = Router();
+
+router
+  .use(authenticateJwt);
+
+router
+  .get("/getmany", getManyPets)
+  .get("/get/:id", getPet)
+  .post("/create", createPet)
+  .patch("/update", updatePet)
+  .delete("/delete", deletePet)
+
+export default router;
