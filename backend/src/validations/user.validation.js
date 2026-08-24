640148ec48531fcd9c53bc592f4626a444ce8c95
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-08-24 01:31:23 -0400

    fix: fixed incorrect RUT validation

--- backend/src/validations/user.validation.js
+++ backend/src/validations/user.validation.js
@@ -36,7 +36,7 @@ export const userQueryValidation = Joi.object({
     rut: Joi.string()
     .min(9)
     .max(12)
-    .pattern(/^(?:(?:[1-9]\d{0}|[1-2]\d{1})(\.\d{3}){2}|[1-9]\d{6}|[1-2]\d{7}|29\.999\.999|29999999)-[\dkK]$/)
+    .pattern(/^\d{1,2}(.?\d{3}){2}-[\dkK]$/)
     .messages({
       "string.empty": "El rut no puede estar vacío.",
       "string.base": "El rut debe ser de tipo string.",
@@ -108,7 +108,7 @@ export const userBodyValidation = Joi.object({
   rut: Joi.string()
     .min(9)
     .max(12)
-    .pattern(/^(?:(?:[1-9]\d{0}|[1-2]\d{1})(\.\d{3}){2}|[1-9]\d{6}|[1-2]\d{7}|29\.999\.999|29999999)-[\dkK]$/)
+    .pattern(/^\d{1,2}(.?\d{3}){2}-[\dkK]$/)
     .messages({
       "string.empty": "El rut no puede estar vacío.",
       "string.base": "El rut debe ser de tipo string.",
